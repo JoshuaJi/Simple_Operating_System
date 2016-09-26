4c171b113d22bcf85bcc2c9f9a01c170bcc7c42b
JohsuaJi <JiXu204@gmail.com>
2016-09-26 12:03:32 -0400

    Initial Commit

---
+++ OS_assignment_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OS_assignment_1
+//
+//  Created by Xu Ji on 2016-09-26.
+//  Copyright © 2016 Xu Ji. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
