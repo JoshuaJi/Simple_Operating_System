658bac998d5384ec4cd4ac23abf22503c1599260
JohsuaJi <JiXu204@gmail.com>
2016-11-30 21:01:45 -0500

    added assignment 3

---
+++ os_assignment_3/sfs_api.c
@@ -0,0 +1,33 @@
+#include "sfs_api.h"
+#include "disk_emu.h"
+
+void mksfs(int fresh){
+
+}
+int sfs_get_next_file_name(char *fname){
+  return 0;
+}
+int sfs_get_file_size(char* path){
+  return 0;
+}
+int sfs_fopen(char *name){
+  return 0;
+}
+int sfs_fclose(int fileID){
+  return 0;
+}
+int sfs_frseek(int fileID, int loc){
+  return 0;
+}
+int sfs_fwseek(int fileID, int loc){
+  return 0;
+}
+int sfs_fwrite(int fileID, char *buf, int length){
+  return 0;
+}
+int sfs_fread(int fileID, char *buf, int length){
+  return 0;
+}
+int sfs_remove(char *file){
+  return 0;
+}
