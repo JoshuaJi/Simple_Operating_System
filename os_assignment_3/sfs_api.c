3b93e8f17e13b1657d9c152ca68cc367ef53899b
Xu JI <xji12@ds2.CS.McGill.CA>
2016-12-01 17:06:10 -0500

    Dec 1 17:06

--- os_assignment_3/sfs_api.c
+++ os_assignment_3/sfs_api.c
@@ -12,6 +12,7 @@
 #define NUM_OF_FREE_MAP_BLOCK 4 
 
 char* disk_name = "sys_xu_ji";
+int next_file_index;
 
 typedef struct Super_Block{
 	int magic_number;
@@ -23,13 +24,14 @@ typedef struct Super_Block{
 
 typedef struct Inode{
 	int index;
+	int size;
 	int dir_pointer[12];
 	int indir_pointer;
 }Inode;
 
 typedef struct Root_Directory{
 	char *file_name;
-	char *exttension;
+	char *extension;
 	int inode_index;
 	
 }Root_Directory;
@@ -48,6 +50,7 @@ void mksfs(int fresh){
 	inode_table = calloc(NUM_OF_INODE, sizeof(Inode));
 	root_directory_table = calloc(NUM_OF_FILE, sizeof(Root_Directory));
 	free_bit_map = calloc(NUM_OF_BLOCK, sizeof(int)); 
+	next_file_index = 0;
 	if (fresh){
 		super_block->magic_number = 0xACBD0005;
 		super_block->block_size = BLOCK_SIZE;
@@ -61,14 +64,14 @@ void mksfs(int fresh){
 				free_bit_map->free_block[i] = 0;
 			}
 		}
-		 printf("a\n");
-		init_fresh_disk("sfs_xu_ji", BLOCK_SIZE, NUM_OF_BLOCK);
-		 printf("b\n");
+		printf("a\n");
+		init_fresh_disk(disk_name, BLOCK_SIZE, NUM_OF_BLOCK);
+		printf("b\n");
 		write_blocks(0, 1, super_block);
 		write_blocks(1, NUM_OF_INODE_BLOCK, inode_table);
 		write_blocks(1+NUM_OF_INODE_BLOCK, NUM_OF_ROOT_DIR_BLOCK, root_directory_table);
 		write_blocks(1+NUM_OF_INODE_BLOCK+NUM_OF_ROOT_DIR_BLOCK, NUM_OF_FREE_MAP_BLOCK, free_bit_map);
- printf("c\n");
+		printf("c\n");
 	}else{
 		init_disk(disk_name, BLOCK_SIZE, NUM_OF_BLOCK);
 		read_blocks(0, 1, super_block);
@@ -78,30 +81,78 @@ void mksfs(int fresh){
 
 	}
 }
+
 int sfs_get_next_file_name(char *fname){
-  return 0;
+	if ((root_directory_table+next_file_index)->file_name){
+		strdup(fname, (root_directory_table+next_file_index)->file_name);
+		strcat(fname, ".");
+		strcat(fname, (root_directory_table+next_file_index)->extension);
+		next_file_index = next_file_index+1;
+		return (root_directory_table+next_file_index)->inode_index;
+	}
+	return 0;
 }
+
+int search_file_in_root(char *fname){
+	for (int i = 0; i < NUM_OF_FILE; i++){
+		char *current_fname = NULL;
+		if ((root_directory+i)){
+			strdup(current_fname, (root_directory+i)->file_name);
+			strcat(current_fname, ".");
+			strcat(current_fname, (root_directory+i)->extension)
+		} 
+		if (current_fname != NULL){
+			if (strcmp(current_fname, fname)){
+				int inode_i = (root_directory+i)->inode_index;
+				return inode_i
+			}
+		}
+	}
+	return -1;
+}
+
 int sfs_get_file_size(char* path){
-  return 0;
+	int inode_i = search_file_in_root(path);
+	if (inode_i != -1){
+		return (inode_table+inode_i)->size;
+	}
+	return -1;
 }
+
 int sfs_fopen(char *name){
-  return 0;
+	int inode_i = search_file_in_root(name);
+	if (inode_i == -1){
+		/*
+
+
+
+
+		
+		from here
+
+
+
+
+
+		*/
+	}
+	return 0;
 }
 int sfs_fclose(int fileID){
-  return 0;
+	return 0;
 }
 int sfs_frseek(int fileID, int loc){
-  return 0;
+	return 0;
 }
 int sfs_fwseek(int fileID, int loc){
-  return 0;
+	return 0;
 }
 int sfs_fwrite(int fileID, char *buf, int length){
-  return 0;
+	return 0;
 }
 int sfs_fread(int fileID, char *buf, int length){
-  return 0;
+	return 0;
 }
 int sfs_remove(char *file){
-  return 0;
+	return 0;
 }
